8744ada33edff37daa78306acd363725496ddd7e
Banhklo2 <gyrudqkd02@gmail.com>
2025-12-17 11:36:36 +0900

    step2

--- src/day/Main.java
+++ src/day/Main.java
@@ -9,15 +9,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Day day = new Day(); // 객체 생성
 
-        System.out.println("해당 번호를 입력하세요: ");
-        int num = sc.nextInt();
+        System.out.println("프로그램을 시작합니다!");
 
-        if (num < 1 || num > 7){
-            System.out.println("잘못된 입력입니다!");
-            return;
+        while (true) {
+            System.out.println("해당 번호를 입력하세요: ");
+            int num = sc.nextInt();
+
+            if (num < 1 || num > 7){
+                System.out.println("잘못된 입력입니다!");
+                continue;
+            }
+
+            day.printNum(num); // 정상 출력
+            break; // 반복 종료
         }
-
-        day.printNum(num); // 메서드 종료
         sc.close();
     }
 }
