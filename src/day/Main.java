caf4349e0a98fb12e453851a5919a24bc48222f8
Banhklo2 <gyrudqkd02@gmail.com>
2025-12-19 17:05:55 +0900

    step3

--- src/day/Main.java
+++ src/day/Main.java
@@ -13,8 +13,23 @@ public class Main {
 
         while (true) {
             System.out.println("해당 번호를 입력하세요: ");
+
+            if (!sc.hasNextInt()){
+                System.out.println("잘못된 입력입니다!");
+                sc.next();
+                continue;
+            }
+
             int num = sc.nextInt();
 
+            if (num == 0){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            if (num < 0 || num > 7) {
+                System.out.println("잘못된 입력입니다!");
+                continue;
+            }
             if (day.printNum(num)) {
                 break; // 정상 출력이면 종료
             }
